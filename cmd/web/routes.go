3c55b71b88b0ccd8c14f3f422334f45b337fab5c
turamant <tur1amant@gmail.com>
2023-06-01 18:06:29 +0300

    dsn OpenDB

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
+	mux.HandleFunc("/download", app.downloadHandler)
 	
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
