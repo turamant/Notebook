9e7074e1fa3921793aaa7087fa9ff79af12b400c
turamant <tur1amant@gmail.com>
2023-06-01 18:22:56 +0300

    add snippets into application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"askvart.ru/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -32,6 +34,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
