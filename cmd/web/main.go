1489ee5165bb29bcb15a12b92766168c1f158d57
turamant <tur1amant@gmail.com>
2023-06-01 13:21:00 +0300

    infoLogTerminal

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,9 +10,17 @@ import (
 func main(){
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
-     
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+    
+    f, e := os.OpenFile("/tmp/info2.log", os.O_RDWR|os.O_CREATE, 0666)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer f.Close()
+
+	infoLogFile := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+	errorLogFile := log.New(f, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+    infoLogTerminal := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -23,7 +31,15 @@ func main(){
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr,mux)
-	errorLog.Fatal(err)
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLogFile,
+		Handler: mux,
+	}
+
+	infoLogFile.Printf("Starting server on %s", *addr)
+	infoLogTerminal.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLogFile.Fatal(err)
+	
 }
