f956e32f41235858d0512626b0cdf6f21de0e17b
turamant <tur1amant@gmail.com>
2023-06-01 11:54:29 +0300

    infoLog and errorLog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main(){
 	addr := flag.String("addr", ":4000", "HTTP network address")
-    flag.Parse()
+	flag.Parse()
+     
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -18,7 +23,7 @@ func main(){
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Запускаем сервер на %s порту", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr,mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
