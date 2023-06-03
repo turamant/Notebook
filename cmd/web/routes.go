a21f0a68730ccf49aae84643afab6914f5965d9d
turamant <tur1amant@gmail.com>
2023-06-03 15:30:20 +0300

    func(logRequest) -> middleware.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,7 +4,7 @@ package main
 import "net/http"
 
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -14,5 +14,5 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	
-	return mux
+	return secureHeaders(mux)
 }
